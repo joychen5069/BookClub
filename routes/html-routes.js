eaa09141c722f0496a078174db1ffd95e5f50eb8
Joy Chen <joychen5069@gmail.com>
2020-05-20 19:59:52 -0400

    changed server.js to ES6 and changed some html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,7 @@
 var path = require("path");
 
 // Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+// var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
 
@@ -24,8 +24,8 @@ module.exports = function(app) {
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
+//   app.get("/members", isAuthenticated, function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/members.html"));
+//   });
 
 };
