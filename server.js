00169d7d372b97f36b7e7a2f53ceabc13c88ae2d
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-05-20 20:57:39 -0400

    fixing config issues

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require("express");
 var session = require("express-session");
 // Requiring passport as we've configured it
-var passport = require("./config/passport");
+
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 8080;
@@ -15,11 +15,11 @@ app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(bookclub.initialize());
+// app.use(bookclub.session());
 
 
-require('dotenv').config()
+
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
