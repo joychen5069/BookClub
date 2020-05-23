3b181f220de76b40de3f307153e7f9df468a4064
Joy Chen <joychen5069@gmail.com>
2020-05-23 11:56:29 -0400

    added file to create table

---
+++ models/books.js
@@ -0,0 +1,45 @@
+module.exports = function(sequelize, DataTypes) {
+    var books = sequelize.define("books", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      author: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      votes: {
+        type: DataTypes.INT,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+    });
+  
+    books.associate = function(models) {
+      // We're saying that a books should belong to an Author
+      // A books can't be created without an Author due to the foreign key constraint
+      books.belongsTo(models.Author, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    return books;
+  };
+  
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -17,3 +17,4 @@ module.exports = (app) => {
     res.render
 
 });
+}
