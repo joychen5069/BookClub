2ddc360f4a7b962b9bf767d7175641b95a67b82d
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-06-01 19:38:37 -0400

    updating schema for clubs table, adding description on clubs page

--- db/schema.sql
+++ db/schema.sql
@@ -7,15 +7,20 @@ USE book_club;
 CREATE TABLE clubs (
 id int AUTO_INCREMENT NOT NULL,
 clubName VARCHAR(30) NOT NULL,
+clubCreator VARCHAR(30) NOT NULL,
+description VARCHAR (500) NOT NULL,
+currentlyReading VARCHAR (30),
 PRIMARY KEY(id)
 );
 
-INSERT INTO clubs (clubName)
-VALUES ("Club One");
-INSERT INTO clubs (clubName)
-VALUES ("Club Two");
-INSERT INTO clubs (clubName)
-VALUES ("Club Three");
+INSERT INTO clubs (clubName, clubCreator, description, currentlyReading)
+VALUES ("Club One", "Joy Chen", "This is a really cool club", "Big Little Lies");
+INSERT INTO clubs (clubName, clubCreator, description, currentlyReading)
+VALUES ("Club Two", "Lauren Cameron", "This is an awesome club", "Little Fires Everywhere");
+INSERT INTO clubs (clubName, clubCreator, description, currentlyReading)
+VALUES ("Club Three", "Cate Dunn", "This is a fun club", "Where the Crawdads Sing");
+INSERT INTO clubs (clubName, clubCreator, description, currentlyReading)
+VALUES ("Club Four", "Tiffany Casey", "This is an amazing club", "Untamed");
 
 CREATE TABLE books (
 id int AUTO_INCREMENT NOT NULL,
