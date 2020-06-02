3afae5e13979d0c086ec1a90b17777a9e8cceacf
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-06-01 20:09:28 -0400

    google images API now working

--- public/js/externalAPI.js
+++ public/js/externalAPI.js
@@ -23,12 +23,15 @@ $(document).ready(function() {
         Object.keys(tempObject).map((key) => {
         let value = tempObject[key];
 
-        //  ----------- THESE TWO NOT PULLING - WORKING ON IT NOW ----------- 
+        let title;
+        let author;
+        let description;
+
           // Returns the value of the rank 
           key === "rank"?
           // let rank = tempObject.rank; 
           console.log("Rank:", tempObject.rank):null
-
+       
           // Returns the value of the the Amazon URL
           key === "amazon_product_url"?
           // let amazon = tempObject.amazon_product_url; 
@@ -36,15 +39,31 @@ $(document).ready(function() {
 
           // Returns the value of the book details
           key === "book_details"?(Object.values(value).map((key3) => {
-          let title = key3.title;
+          title = key3.title;
           console.log("Title", title)
-          let author = key3.author;
+          author = key3.author;
           console.log("Author", author)
-          let description = key3.description;
+          description = key3.description;
           console.log("Description", description)
-          let isbn10 = key3.primary_isbn10;
-          console.log("ISBN10", isbn10)
+          
+          // checking to see if there is an isbns - if so, it will add to the Google API
+          key3["primary_isbn10"]? (googleAPI(key3.primary_isbn10)):null
+          console.log("ISBN10 in NYT API", key3.primary_isbn10)
           })):null // end of Object.Values for book_details
+
+              // just put this here so you can easily see the variables to include 
+            var listing =
+            "<div class='col s12 m6'>"
+            // + rank  need to get this to work 
+            // + title
+            // + author
+            // + description
+            // // + img
+            // + amazon need to get this to work
+            + "</div>";
+
+            $('#best-seller-titles').append(listing);
+
 
           }) // end of Object.keys (tempObject)
         }) // end of Object.keys(results)
@@ -52,38 +71,15 @@ $(document).ready(function() {
       .catch(error => {
       console.log('NYT API Error');
     });
-
-
-
-
-    //----------- LAUREN ADD IN CARD HERE ----------- 
-
-
-
-
-  //----------- NEED TO GET THESE TO PULL ----------- 
-// just put this here so you can easily see the variables to include 
-    var listing =
-    "<div class='col s12 m6'>"
-    // + rank  need to get this to work 
-    // + title
-    // + author
-    // + description
-    // // + img
-    // + amazon need to get this to work
-    + "</div>";
-
-    $('#best-seller-titles').append(listing);
-
   } // end of NYT API 
 
   // GOOGLE API 
-  function googleAPI() { 
+  function googleAPI(isbn10) { 
     const GOOGLE_API_URL= 'https://www.googleapis.com/books/v1/volumes?q=isbn:';
     const GOOGLE_API_KEY = '&key=AIzaSyA6uNRyxzKhz1rSMQZYAu4wnaE4CvTMITs';
-    // console.log("ISBN10_______________", isbn10)
+    console.log("ISBN10_______________", isbn10)
 
-    const GOOGLE_ENTIRE_API_URL = `${GOOGLE_API_URL}${"0735219117"}${GOOGLE_API_KEY}`;
+    const GOOGLE_ENTIRE_API_URL = `${GOOGLE_API_URL}${isbn10}${GOOGLE_API_KEY}`;
     console.log("GOOGLE URL", GOOGLE_ENTIRE_API_URL)
     
     fetch(`${GOOGLE_ENTIRE_API_URL}`, {
@@ -104,6 +100,5 @@ $(document).ready(function() {
 nyTimes();
 
 // Calls the Google API function to run
-googleAPI();
 
 }); // Final closing tag 
