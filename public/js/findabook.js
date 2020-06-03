1eef8a890d49b02848686ba66fc0dc14fbb71b91
Joy Chen <joychen5069@gmail.com>
2020-06-03 13:27:34 -0400

    fixed findabook.js so that when you search for a new book, it replaces the previously searched book instead of adding to it

--- public/js/findabook.js
+++ public/js/findabook.js
@@ -7,8 +7,8 @@ const selectedBook = (title, author, description, img) => {
     '<div class="col m6 s12">' +
       '<p class="textstyle2">' + title +' by ' + author +
       '</p> <p class="textstyle2">' + description + '</p> </div>' +
-      '<div class="col m6 s12">' + img +
-    //   '<img src=' + img + '>' + // this is throwing an error for me 
+      // '<div class="col m6 s12">' + img +
+      '<img src=' + img + '>' + // this is throwing an error for me 
       '</div> </div>';
 
       $('#searched-books').empty().append(selectedBookHTML);
