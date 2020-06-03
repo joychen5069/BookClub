f92adfa69b779d703081dc316e0aa86ca702f057
Joy Chen <joychen5069@gmail.com>
2020-06-03 19:24:01 -0400

    pushing to pull

--- models/clubs.js
+++ models/clubs.js
@@ -2,33 +2,33 @@ const orm = require('../config/orm')
 
 const club = {
   //show all the burgers
-    selectAll: (cb) => {
-      orm.selectAll("clubs", (res) => {
-        cb(res);
-      });
-    },
+  selectAll: (cb) => {
+    orm.selectAll("clubs", (res) => {
+      cb(res);
+    });
+  },
 
-    //add a new burger into the to be eaten pile
-    insertOne: (cols, vals, cb) => {
-      orm.insertOne("clubs", cols, vals, (res) => {
-        cb(res);
-      });
-    },
-    
-    delete: (condition, cb) => {
-      orm.delete("clubs", condition, (res) => {
-        cb(res);
-      });
-    },
+  //add a new burger into the to be eaten pile
+  insertOne: (cols, vals, cb) => {
+    orm.insertOne("clubs", cols, vals, (res) => {
+      cb(res);
+    });
+  },
 
-    selectByID: (id, cb) => {
-      orm.selectbyId("clubs", id, (res) => {
-        cb(res);
-      });
-    },
-    
-    
-  };
-  
-  // Export the database functions for the controller (clubsController.js).
-  module.exports = club;
+  delete: (condition, cb) => {
+    orm.delete("clubs", condition, (res) => {
+      cb(res);
+    });
+  },
+
+  selectByID: (id, cb) => {
+    orm.selectbyId("clubs", id, (res) => {
+      cb(res);
+    });
+  },
+
+
+};
+
+// Export the database functions for the controller (clubsController.js).
+module.exports = club;
