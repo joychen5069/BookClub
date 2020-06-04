989462c1ca4ca8676c5f159e7af07f5204a84baa
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-06-04 10:34:23 -0400

    deleting un needed comments

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -25,18 +25,6 @@ router.get("/clubs", (req, res) => {
   });
 });
 
-//Create route to add a club to list
-// router.post("/api/clubs", (req, res) => {
-//    (req.body.name)
-//   club.insertOne([
-//     "clubName"
-//   ], [
-//     req.body.name
-//   ], (result) => {
-//     res.json({ id: result.insertId });
-    
-//   });
-// });
 
 //Create route to get club by id
 router.get("/clubs/:id", function(req, res) {
