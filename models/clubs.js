75e07862c7dff26275bb74fe0a16148314dc601b
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-06-04 11:19:25 -0400

    cleaning comments in club.js file

--- models/clubs.js
+++ models/clubs.js
@@ -1,32 +1,35 @@
 const orm = require('../config/orm')
 
 const club = {
-  //show all the burgers
+  //show all the clubs
     selectAll: (cb) => {
       orm.selectAll("clubs", (res) => {
         cb(res);
       });
     },
 
-    //add a new burger into the to be eaten pile
+    //add a new club
     insertOne: (cols, vals, cb) => {
       orm.insertOne("clubs", cols, vals, (res) => {
         cb(res);
       });
     },
     
+    //delete a club - not using
     delete: function(condition, cb) {
       orm.delete("clubs", condition, (res) => {
         cb(res);
       });
     },
 
+    //select a club by ID
     selectByID: (id, cb) => {
       orm.selectbyId("clubs", id, (res) => {
         cb(res);
       });
     },
     
+    //update the book a club is reading
     update: (data, condition, cb) => {
       console.log("data", data, "condition", condition)
       orm.update(data, condition, (res) => {
